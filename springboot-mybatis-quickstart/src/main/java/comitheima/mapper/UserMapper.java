17f87bb1a84a45b98e5ca3e261d9ffcee1ea625a
ptx <3351626369@qq.com>
2026-05-15 20:33:01 +0800

    XML配置及辅助配置

--- springboot-mybatis-quickstart/src/main/java/comitheima/mapper/UserMapper.java
+++ springboot-mybatis-quickstart/src/main/java/comitheima/mapper/UserMapper.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 @Mapper//应用程序在运行时，会自动的为该接口创建一个实现类对象，该对象会交给IOC进行管理
 public interface UserMapper {
-    @Select("select * from user")
+   //@Select("select * from user")
     public List<User> findAll();//查询返回值为User集合
 
     @Delete("delete from user where id=#{id}")
